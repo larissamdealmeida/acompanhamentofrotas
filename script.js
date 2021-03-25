0469cfb382adc7e50c7525b81c80dbef2fe757a1
larissamdealmeida <76062664+larissamdealmeida@users.noreply.github.com>
2021-03-25 11:22:07 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // function([string1, string2],target id,[color1,color2])    
- consoleText(['EQUIPE COMERCIAL.', 'VAREJO'], 'text',['papayawhip','ghostwhite','lightblue']);
+ consoleText(['ACOMPANHAMENTO FROTAS.', 'VAREJO'], 'text',['papayawhip','ghostwhite','lightblue']);
 
 function consoleText(words, id, colors) {
   if (colors === undefined) colors = ['#fff'];
